ba955d2ca497a32b8ec3f2f7e2f0302ae16b3bd4
Florian Ragwitz <rafl@debian.org>
2007-08-10 17:23:02 +0200

    Cleanup tabs.

--- lib/Config/INI/Reader.pm
+++ lib/Config/INI/Reader.pm
@@ -90,18 +90,18 @@ Given a filename, this method returns a hashref of the contents of that file.
 =cut
 
 sub read_file {
-	my ($invocant, $filename) = @_;
+    my ($invocant, $filename) = @_;
 
-	# Check the file
-  Carp::croak "no filename specified" unless $filename;
-  Carp::croak "file '$filename' does not exist" unless -e $filename;
-	Carp::croak "'$filename' is not a plain file" unless -f _;
+    # Check the file
+    Carp::croak "no filename specified" unless $filename;
+    Carp::croak "file '$filename' does not exist" unless -e $filename;
+    Carp::croak "'$filename' is not a plain file" unless -f _;
 
-	# Slurp in the file
-  my $handle = IO::File->new($filename, '<')
+    # Slurp in the file
+    my $handle = IO::File->new($filename, '<')
     or Carp::croak "couldn't read file '$filename': $!";
 
-	$invocant->read_handle($handle);
+    $invocant->read_handle($handle);
 }
 
 =head2 read_string
@@ -114,13 +114,13 @@ Given a string, this method returns a hashref of the contents of that string.
 
 # Create an object from a string
 sub read_string {
-	my ($invocant, $string) = @_;
+    my ($invocant, $string) = @_;
 
-  Carp::croak "no string provided" unless defined $string;
+    Carp::croak "no string provided" unless defined $string;
 
-  my $handle = IO::String->new($string);
+    my $handle = IO::String->new($string);
 
-  $invocant->read_handle($handle);
+    $invocant->read_handle($handle);
 }
 
 =head2 read_handle
@@ -133,40 +133,40 @@ handle.
 =cut
 
 sub read_handle {
-  my ($invocant, $handle) = @_;
+    my ($invocant, $handle) = @_;
 
-  my $self = ref $invocant ? $invocant : $invocant->new;
+    my $self = ref $invocant ? $invocant : $invocant->new;
 
-	# parse the file
-  LINE: while (local $_ = $handle->getline) {
-		# Skip comments and empty lines
-		next LINE if /\A\s*(?:\#|\;|$)/;
+    # parse the file
+    LINE: while (local $_ = $handle->getline) {
+        # Skip comments and empty lines
+        next LINE if /\A\s*(?:\#|\;|$)/;
 
-		# Remove inline comments
-		s/\s+#\s.+$//g;
+        # Remove inline comments
+        s/\s+#\s.+$//g;
 
-		# Handle section headers
-		if ( /^\s*\[\s*(.+?)\s*\]\s*$/ ) {
-			# Create the sub-hash if it doesn't exist.
-			# Without this sections without keys will not
-			# appear at all in the completed struct.
-      $self->change_section($1);
-			next LINE;
-		}
+        # Handle section headers
+        if ( /^\s*\[\s*(.+?)\s*\]\s*$/ ) {
+            # Create the sub-hash if it doesn't exist.
+            # Without this sections without keys will not
+            # appear at all in the completed struct.
+            $self->change_section($1);
+            next LINE;
+        }
 
-		# Handle properties
-		if ( /^\s*([^=]+?)\s*=\s*(.*?)\s*$/ ) {
-			$self->set_value($1, $2);
-			next;
-		}
+        # Handle properties
+        if ( /^\s*([^=]+?)\s*=\s*(.*?)\s*$/ ) {
+            $self->set_value($1, $2);
+            next;
+        }
 
     my $lineno = $handle->input_line_number;
-		Carp::croak "Syntax error at line $lineno: '$_'";
-	}
+        Carp::croak "Syntax error at line $lineno: '$_'";
+    }
 
-  $self->finalize;
+    $self->finalize;
 
-	return $self->{data};
+    return $self->{data};
 }
 
 =head2 current_section
@@ -179,7 +179,7 @@ been set, it returns the result of calling the C<starting_section> method.
 =cut
 
 sub current_section {
-  defined $_[0]->{section} ? $_[0]->{section} : $_[0]->starting_section
+    defined $_[0]->{section} ? $_[0]->{section} : $_[0]->starting_section
 }
 
 =head2 change_section
@@ -194,10 +194,10 @@ being read and to initialize that section to an empty hashref.
 =cut
 
 sub change_section {
-  my ($self, $section) = @_;
+    my ($self, $section) = @_;
 
-  $self->{section} = $section;
-  $self->{data}{$section} ||= {};
+    $self->{section} = $section;
+    $self->{data}{$section} ||= {};
 }
 
 =head2 set_value
@@ -210,9 +210,9 @@ behavior is to change the value of the named property to the given value.
 =cut
 
 sub set_value {
-  my ($self, $name, $value) = @_;
+    my ($self, $name, $value) = @_;
 
-  $self->{data}{ $self->current_section }{ $name } = $value;
+    $self->{data}{ $self->current_section }{ $name } = $value;
 }
 
 =head2 starting_section
@@ -247,45 +247,45 @@ ephemerally.
 =cut
 
 sub new {
-  my ($class) = @_;
+    my ($class) = @_;
 
-  my $self = {
-    data    => {},
-  };
+    my $self = {
+        data    => {},
+    };
 
-  bless $self => $class;
+    bless $self => $class;
 }
 
 # # Save an object to a file
 # sub write {
-# 	my $self = shift;
-# 	my $file = shift or return $self->_error(
-# 		'No file name provided'
-# 		);
+#     my $self = shift;
+#     my $file = shift or return $self->_error(
+#         'No file name provided'
+#         );
 # 
-# 	# Write it to the file
-# 	open( CFG, '>' . $file ) or return $self->_error(
-# 		"Failed to open file '$file' for writing: $!"
-# 		);
-# 	print CFG $self->write_string;
-# 	close CFG;
+#     # Write it to the file
+#     open( CFG, '>' . $file ) or return $self->_error(
+#         "Failed to open file '$file' for writing: $!"
+#         );
+#     print CFG $self->write_string;
+#     close CFG;
 # }
 # 
 # # Save an object to a string
 # sub write_string {
-# 	my $self = shift;
+#     my $self = shift;
 # 
-# 	my $contents = '';
-# 	foreach my $section ( sort { (($b eq '_') <=> ($a eq '_')) || ($a cmp $b) } keys %$self ) {
-# 		my $block = $self->{$section};
-# 		$contents .= "\n" if length $contents;
-# 		$contents .= "[$section]\n" unless $section eq '_';
-# 		foreach my $property ( sort keys %$block ) {
-# 			$contents .= $self->property_string($section, $property);
-# 		}
-# 	}
-# 	
-# 	$contents;
+#     my $contents = '';
+#     foreach my $section ( sort { (($b eq '_') <=> ($a eq '_')) || ($a cmp $b) } keys %$self ) {
+#         my $block = $self->{$section};
+#         $contents .= "\n" if length $contents;
+#         $contents .= "[$section]\n" unless $section eq '_';
+#         foreach my $property ( sort keys %$block ) {
+#             $contents .= $self->property_string($section, $property);
+#         }
+#     }
+#     
+#     $contents;
 # }
 # 
 # sub property_string { "$_[2]=$_[0]->{$_[1]}->{$_[2]}\n" };
