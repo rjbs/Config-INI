b0efa4d41adcbf3f08a10c1c7e1d23ee36f6c2da
Ricardo SIGNES <rjbs@cpan.org>
2007-11-04 22:14:26 -0500

    close more roundtripping problems

--- lib/Config/INI/Reader.pm
+++ lib/Config/INI/Reader.pm
@@ -228,7 +228,7 @@ returns false.
 =cut
 
 sub parse_value_assignment {
-  return ($1, $2) if $_[1] =~ /^\s*([^=]+?)\s*=\s*(.*?)\s*$/;
+  return ($1, $2) if $_[1] =~ /^\s*([^=\s][^=]*?)\s*=\s*(.*?)\s*$/;
   return;
 }
 
