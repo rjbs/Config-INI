490d11895289e7f2912d8c711cd90b90473a877e
George Hartzell <hartzell@alerce.com>
2013-09-25 21:29:32 -0700

    Don't return regexps, use affirmative predicate tests
    Change routines that were returning regular expressions that client
    code then needed to Do The Right Thing with into predicates that test
    whether a thing is valid or not.  (Based on Ricardo's feedback).

--- lib/Config/INI/Writer.pm
+++ lib/Config/INI/Writer.pm
@@ -196,32 +196,41 @@ sub preprocess_input {
   return \@new_data;
 }
 
-=head2 invalid_section_name_regexp
+=head2 validate_section_name
 
   Carp::croak "section name contains illegal character"
-    if $name =~ $writer->invalid_section_name_regexp();
+    if not $writer->is_valid_section_name($name);
 
 =cut
 
-sub invalid_section_name_regexp { qr/(?:\n|\s;|^\s|\s$)/ }
+sub is_valid_section_name {
+    my ($self, $name) = @_;
+    return $name !~ qr/(?:\n|\s;|^\s|\s$)/;
+}
 
-=head2 invalid_property_name_regexp
+=head2 is_valid_property_name
 
   Carp::croak "property name contains illegal character"
-    if $name =~ $writer->invalid_property_name_regexp();
+    if not $writer->is_valid_property_name($name);
 
 =cut
 
-sub invalid_property_name_regexp { qr/(?:\n|\s;|^\s|\s|=$)/ }
+sub is_valid_property_name {
+    my ($self, $property) = @_;
+    return $property !~ qr/(?:\n|\s;|^\s|\s|=$)/;
+}
 
-=head2 invalid_value_regexp
+=head2 is_valid_value
 
   Carp::croak "value contains illegal character"
-    if $value =~ $writer->invalid_value_regexp();
+    if not $writer->is_valid_value($name);
 
 =cut
 
-sub invalid_value_regexp { qr/(?:\n|\s;|^\s|\s$)/ }
+sub is_valid_value {
+    my ($self, $value) = @_;
+    return $value !~ qr/(?:\n|\s;|^\s|\s$)/;
+}
 
 =head2 validate_input
 
@@ -247,17 +256,17 @@ sub validate_input {
     $seen{ $name } ||= {};
 
     Carp::croak "illegal section name '$name'"
-      if $name =~ $self->invalid_section_name_regexp;
+      if not $self->is_valid_section_name($name);
 
     for (my $j = 0; $j < $#$props; $j += 2) {
       my $property = $props->[ $j ];
       my $value    = $props->[ $j + 1 ];
 
       Carp::croak "property name '$property' contains illegal character"
-        if $property =~ $self->invalid_property_name_regexp;
+        if not $self->is_valid_property_name($property);
 
       Carp::croak "value for $name.$property contains illegal character"
-        if defined $value and $value =~ $self->invalid_value_regexp;
+        if defined $value and not $self->is_valid_value($value);
 
       if ( $seen{ $name }{ $property }++ ) {
         Carp::croak "multiple assignments found for $name.$property";
